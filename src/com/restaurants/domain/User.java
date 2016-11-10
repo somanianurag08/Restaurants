6283ab65ff14192ce75115f854d2905aea447a5f
anurag <somanianurag@hotmail.com>
2016-11-10 08:54:36 +0530

    User Domain

---
+++ src/com/restaurants/domain/User.java
@@ -0,0 +1,49 @@
+package com.restaurants.domain;
+
+import java.util.Date;
+
+public class User {
+
+	private long id;
+	private String userName;
+	private String address;
+	private String mobile;
+	private Date dob;
+	private String email;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
