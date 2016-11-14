a99a32f30784eac461e916df56e465c851741282
anurag <somanianurag@hotmail.com>
2016-11-14 21:50:04 +0530

    Web Services

--- src/com/restaurants/domain/User.java
+++ src/com/restaurants/domain/User.java
@@ -2,47 +2,84 @@ package com.restaurants.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "USER")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
 	private long id;
+
+	@Column(name = "USERNAME")
 	private String userName;
+
+	@Column(name = "ADDRESS")
 	private String address;
+
+	@Column(name = "MOBILE")
 	private String mobile;
+
+	@Column(name = "DOB")
+	@Temporal(TemporalType.DATE)
 	private Date dob;
+
+	@Column(name = "EMAIL")
 	private String email;
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getMobile() {
 		return mobile;
 	}
+
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
+
 	public Date getDob() {
 		return dob;
 	}
+
 	public void setDob(Date dob) {
 		this.dob = dob;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
